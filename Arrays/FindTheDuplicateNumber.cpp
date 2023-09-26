b2d6f5329f4f2186ab62f4f97a4ab6a61b3190da
Tanishq Chauhan <66708936+tanishq1200@users.noreply.github.com>
2023-09-26 11:43:26 +0530

    Update FindTheDuplicateNumber.cpp

--- Arrays/FindTheDuplicateNumber.cpp
+++ Arrays/FindTheDuplicateNumber.cpp
@@ -1,1 +1,12 @@
+//Leectode - https://leetcode.com/problems/find-the-duplicate-number/description/
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        while(nums[0] != nums[nums[0]]){
+            swap(nums[0], nums[nums[0]]);
+        }
+        return nums[0];
+        
+    }
 
+};
