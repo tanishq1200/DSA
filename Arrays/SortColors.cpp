88e7022afa13e7463705c550950ce3747c922051
Tanishq Chauhan <66708936+tanishq1200@users.noreply.github.com>
2023-09-26 09:09:14 +0530

    Update SortColors.cpp

--- Arrays/SortColors.cpp
+++ Arrays/SortColors.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l = 0;
+        int m = 0;
+        int h = nums.size()-1;
+        while(m<=h){
+            if(nums[m] == 0){
+                swap(nums[l],nums[m]);
+                l++;
+                m++;
+            }
+            else if(nums[m] == 1){
+                m++;
+            }
+            else{
+                swap(nums[h],nums[m]);
+                h--;
+            }
+        }
+    }
+};
