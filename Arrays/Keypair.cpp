9699939d0413f77e1344bf9c539ebfc09d1f3c21
tanishq1200 <66708936+tanishq1200@users.noreply.github.com>
2023-09-26 08:20:50 +0530

    Update Keypair.cpp

--- Arrays/Keypair.cpp
+++ Arrays/Keypair.cpp
@@ -1,1 +1,19 @@
-
+class Solution{
+public:	
+	bool twopointer(int arr[] , int n, int x){
+        int start = 0;
+        int last = n-1;
+        while(start < last){
+            int sum = arr[start] + arr[last];
+            if(sum == x) return true;
+            else if(sum > x)  last --;
+            else start ++;
+        }
+        return false;
+    }
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    sort(arr, arr+n);
+        return twopointer(arr,n,x);
+	    
+	}
+};
