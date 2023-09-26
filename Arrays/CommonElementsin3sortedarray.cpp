5ba56c24945a0d3042958b28b1d394450ffaa53a
Tanishq Chauhan <66708936+tanishq1200@users.noreply.github.com>
2023-09-26 12:13:43 +0530

    Update CommonElementsin3sortedarray.cpp

--- Arrays/CommonElementsin3sortedarray.cpp
+++ Arrays/CommonElementsin3sortedarray.cpp
@@ -1,1 +1,33 @@
-
+ vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+        {
+            //code here.
+            vector<int>ans;
+            set<int>s;
+            int num = 0;
+            int i,j,k;
+            i=j=k=0;
+            while(i<n1 && j<n2 && k<n3){
+                
+                if(A[i] == B[j] && B[j] == C[k]){
+                    
+                    s.insert(A[i]);
+                    i++,j++,k++;
+                }
+                else if(A[i] < B[j]){
+                    i++;
+                }
+                
+                else if(B[j] < C[k]){
+                    j++;
+                }
+                
+                else{
+                    k++;
+                }
+                
+        }
+        for(auto i:s){
+            ans.push_back(i);
+        }
+        return ans;
+        }
