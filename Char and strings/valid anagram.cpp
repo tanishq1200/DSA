0bca0d5b3b1c24a6d710c252a8dc303eaa9d6857
Tanishq Chauhan <66708936+tanishq1200@users.noreply.github.com>
2023-10-03 08:05:01 +0530

    Create valid anagram.cpp

---
+++ Char and strings/valid anagram.cpp
@@ -0,0 +1,19 @@
+//Leetcode - https://leetcode.com/problems/valid-anagram/
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int freqtable[256] = {0};
+        for(int i = 0; i < s.size(); i++){
+            freqtable[s[i]]++;
+        }
+        for(int i = 0; i < t.size(); i++){
+            freqtable[t[i]]--;
+        }
+        for(int i = 0; i < 256; i++){
+            if(freqtable[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
