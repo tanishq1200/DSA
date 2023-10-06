ef3decfdb9633bbc96d2e76537b3beabfa17d725
Tanishq Chauhan <66708936+tanishq1200@users.noreply.github.com>
2023-10-06 09:42:40 +0530

    Update Integer to Roman.cpp

--- Char and strings/Integer to Roman.cpp
+++ Char and strings/Integer to Roman.cpp
@@ -6,7 +6,7 @@ public:
         int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
         string ans= "";
-        int i =0;
+        
 
         for(int i =0; i < 13; i++){
             while(num >= values[i]){
